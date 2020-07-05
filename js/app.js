2b317e9812f8f19ccbff63e02adce327036f413d
Felix Weickert <felix.weickert@dws.co.at>
2020-07-05 23:41:30 +0200

    feat: dynamic landing page
    dynamic navbar showing active section and allowing scroll to content

--- js/app.js
+++ js/app.js
@@ -17,14 +17,64 @@
  * Define Global Variables
  * 
 */
-
-
+let navBarList = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll("section");
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function populateNavBar(){
+    const myDocFrag = document.createDocumentFragment();
 
+    for (section of sections){
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.setAttribute("href","#" + section.id);
+        link.innerHTML = section.getAttribute("data-nav");
+        link.className = "menu__link";
+        listItem.appendChild(link);
+        listItem.addEventListener('click',scrollToPosition);
+        myDocFrag.appendChild(listItem);
+        addScrollListener(section);
+    }
+    navBarList.appendChild(myDocFrag);
+    updateNavBar(sections[0]);
+}
+//es ist nicht ganz klar wie man scrollen soll -> event listener für anchor? -> default behavior unterdrücken? -> id von anchor nehmen und mit $('#id').ScrollTo(); hinscrollen
+function scrollToPosition(event){
+    event.preventDefault();
+    const element = document.querySelector(event.target.getAttribute("href"));
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function addScrollListener(tolookfor){
+    window.addEventListener('scroll', function() {
+        var element = tolookfor;
+        var position = element.getBoundingClientRect();
+        if(position.top >= 0 && position.top < 400) {
+            updateNavBar(element);
+        }
+    });
+}
+
+
+function updateNavBar(target){
+    const navBarItems = navBarList.querySelectorAll("li");
+    for(navItem of navBarItems){
+        let link = navItem.querySelector("a");
+        if (link.innerHTML == target.getAttribute("data-nav")){
+            link.classList.remove("menu__link");
+            link.classList.add("menu__link__selected");
+        } else{
+            link.classList.add("menu__link");
+            link.classList.remove("menu__link__selected");
+        }
+    }
+}
+
+
+populateNavBar();
 
 
 /**
